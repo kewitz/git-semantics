9c8d3f10514706a4510df5c4f92e9386881d56bc
Keenahn Jung <keenahn+github@gmail.com>
2016-05-31 20:08:30 -0700

    feat: add refact and merge

--- commit-emoji.sh
+++ commit-emoji.sh
@@ -1,15 +1,15 @@
-!#/bin/bash
 #!/bin/bash
 
 VALUE=$(cat)
 
-echo "$VALUE" | \
+printf "$VALUE" | \
   sed 's/feat:/🎉  feat:/ ' | \
   sed 's/fix:/🐞  fix:/' | \
   sed 's/refactor:/🌀  refactor:/' | \
+  sed 's/refact:/🌀  refactor:/' | \
   sed 's/chore:/🔧  chore:/' | \
   sed 's/docs:/📚  docs:/' | \
   sed 's/style:/👔  style:/' | \
   sed 's/test:/🍏  test:/' | \
-  sed 's/Merge/🌱  Merge/'
-
+  sed 's/Merge/🌱  Merge/' | \
+  sed 's/merge:/🌱  merge:/'
